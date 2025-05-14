2d4867e0a2f8b6c9bc7ae22bc1228fa77efa2c72
LuisSancho64 <kidbucle100@gmail.com>
2025-05-14 11:48:36 -0500

    Ubicando imagenes en tarjetas de home con animacion y reubicando estilo

--- Client/obj/Debug/net8.0/SMI.Client.AssemblyInfo.cs
+++ Client/obj/Debug/net8.0/SMI.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SMI.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e31ee202494d3ff52e5980133bafdd843e7b7a8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd7ceedc2cd974d17008828674c7f889eb86d52a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SMI.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SMI.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Shared/obj/Debug/net8.0/SMI.Shared.AssemblyInfo.cs
+++ Shared/obj/Debug/net8.0/SMI.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SMI.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e31ee202494d3ff52e5980133bafdd843e7b7a8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd7ceedc2cd974d17008828674c7f889eb86d52a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SMI.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SMI.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
