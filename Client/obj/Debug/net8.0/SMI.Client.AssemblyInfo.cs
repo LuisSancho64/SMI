fd3c79f8013c52644118ac945e7c57b0058bd20f
LuisSancho64 <kidbucle100@gmail.com>
2025-05-13 23:56:04 -0500

    Creando componente Tarjeta y su estilo

--- Client/obj/Debug/net8.0/SMI.Client.AssemblyInfo.cs
+++ Client/obj/Debug/net8.0/SMI.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SMI.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e87ff41e2c8396f00364772144e680a8af09daed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+353131e27b03284c080197ff6e6d8ff25cb0f0bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("SMI.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SMI.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
