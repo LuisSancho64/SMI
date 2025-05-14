e31ee202494d3ff52e5980133bafdd843e7b7a8e
LuisSancho64 <kidbucle100@gmail.com>
2025-05-14 00:00:43 -0500

    Ubicando las targetas dentro del home

--- Client/obj/Debug/net8.0/SMI.Client.AssemblyInfo.cs
+++ Client/obj/Debug/net8.0/SMI.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SMI.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+353131e27b03284c080197ff6e6d8ff25cb0f0bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd3c79f8013c52644118ac945e7c57b0058bd20f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SMI.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SMI.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Shared/obj/Debug/net8.0/SMI.Shared.AssemblyInfo.cs
+++ Shared/obj/Debug/net8.0/SMI.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SMI.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e87ff41e2c8396f00364772144e680a8af09daed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd3c79f8013c52644118ac945e7c57b0058bd20f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SMI.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SMI.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
