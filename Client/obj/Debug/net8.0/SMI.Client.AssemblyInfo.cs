9a903c6dd48492e4fe676a34ca8415159f63b211
LuisSancho64 <kidbucle100@gmail.com>
2025-05-14 21:56:00 -0500

    Modificando las tarjetas del Home el apartado de Personas y creando pagina en pages para usuarios

--- Client/obj/Debug/net8.0/SMI.Client.AssemblyInfo.cs
+++ Client/obj/Debug/net8.0/SMI.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SMI.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d4867e0a2f8b6c9bc7ae22bc1228fa77efa2c72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b96841924c5bbf1b47619b15e904df8f2a504901")]
 [assembly: System.Reflection.AssemblyProductAttribute("SMI.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SMI.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
