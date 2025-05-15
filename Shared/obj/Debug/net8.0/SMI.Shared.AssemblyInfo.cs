b96841924c5bbf1b47619b15e904df8f2a504901
LuisSancho64 <kidbucle100@gmail.com>
2025-05-14 21:52:59 -0500

    Modificando el Navbar con el apartado nuevo de Usuarios

--- Shared/obj/Debug/net8.0/SMI.Shared.AssemblyInfo.cs
+++ Shared/obj/Debug/net8.0/SMI.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SMI.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd7ceedc2cd974d17008828674c7f889eb86d52a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d4867e0a2f8b6c9bc7ae22bc1228fa77efa2c72")]
 [assembly: System.Reflection.AssemblyProductAttribute("SMI.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SMI.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
